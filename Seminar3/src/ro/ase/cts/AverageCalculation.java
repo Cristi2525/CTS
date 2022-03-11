ecbd387e3a8bb57ea6f762305c330178b72a1519
Miu Cristian Robert <miucristian17@stud.ase.ro>
2022-03-11 23:03:39 +0200

    Remove old comment

--- Seminar3/src/ro/ase/cts/AverageCalculation.java
+++ Seminar3/src/ro/ase/cts/AverageCalculation.java
@@ -10,7 +10,6 @@ public class AverageCalculation implements Calculation<Double> {
     public Double calculate(List<Integer> list) {
         if (list != null && list.size() > 0) {
             return list.stream().mapToInt(i -> i).average().getAsDouble();
-           // return list.stream().reduce((a,b) -> a+b ).get()/list.size(); // a doua optiune
         }
         return 0d;
     }
